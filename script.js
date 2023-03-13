0f34cf6e35ff0fbec5d5b6b96fda8842aa58f120
Gading Kinarayana <100770471+Gadingknr@users.noreply.github.com>
2023-03-13 17:10:18 +0800

    move it to different file

---
+++ script.js
@@ -0,0 +1,24 @@
+ /*adding sound after the result
+  */
+ function say(myMessage){
+    var msg = new SpeechSynthesisUtterance(myMessage);
+    window.speechSynthesis.speak(msg);
+  }
+/* This is a simplified version of the game.
+The Computer ALWAYS picks ROCK.  
+Does the user's choose win,lose, or tie vs ROCK? */
+  function play(user) {
+    let result = "";
+    if (user === 'rock') {
+        result = "tie";
+    }
+    if (user === 'paper') {
+      result = "win";
+    }
+    if (user === 'scissors') {
+        result = "lose";
+      }
+    var theMessage = "Computer cjose rock, you " + result + "!"
+    alert(theMessage);
+    say(theMessage);
+  }
